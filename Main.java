abbbe970fcd14edbef78b08fbbaaac5bea8b3822
studentFatmaFat <78883837+FatmaB4@users.noreply.github.com>
2021-10-31 01:35:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import static java.lang.System.*;
+
+public class Main
+{
+	public static void main( String args[] )
+	{
+		System.out.println(StringFirstLetterCheck.checkFirstLetter( "hello","howdy" ));
+		System.out.println(StringFirstLetterCheck.checkFirstLetter( "three","two" ));
+		System.out.println(StringFirstLetterCheck.checkFirstLetter( "TCEA","UIL" ));
+	}
+}
